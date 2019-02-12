c09ac867446f81c2fbcd5556341d193f50efe1e8
Lena <elena.borisenko1991@gmail.com>
2019-02-12 13:43:16 +0000

    add base structure to the project

---
+++ src/index.js
@@ -0,0 +1,31 @@
+(function() {
+  const directives = {};
+  const smallAngular = {
+
+    directive(name, cb) {
+    },
+
+    compile(node) {
+    },
+    bootstrap(node) {
+    }
+  };
+
+  smallAngular.directive('ng-model', function(el) {
+  });
+
+  smallAngular.directive('ng-click', function(el) {
+  });
+
+  smallAngular.directive('ng-show', function(el) {
+  });
+
+  smallAngular.directive('ng-hide', function(el) {
+  });
+
+  smallAngular.directive('make_short', function(el) {
+  });
+
+  window.smallAngular = smallAngular;
+}());
+
